8855f6b681d4b03e16f281021f8713eb5f1a8232
unknown <guilhermekhirata@hotmail.com>
2023-07-11 19:55:38 -0300

    feat: definindo estilos globais

--- my-vacancies/src/styles/global.jsx
+++ my-vacancies/src/styles/global.jsx
@@ -1,5 +1,16 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
-    
+    * {
+        font-family: 'Inter', sans-serif;
+    }
+
+    :root {
+        --color-blue: #5865F2;
+        --color-light-gray: #e4e6d8;
+        --color-medium-gray: #b8b4d1;
+        --color-dark-gray: #7e868d;
+
+        --color-white-fixed: #FFF;
+    }
 `
